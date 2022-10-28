a88553b93764db3ed78fdf43351c0cfbedc755a1
Marco Ilari <marilari88@gmail.com>
2022-10-28 23:55:27 +0200

    Update lspconfig

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -9,7 +9,7 @@ end
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local opts = { noremap = true, silent = true }
 -- vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, opts)
