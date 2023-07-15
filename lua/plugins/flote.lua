48a2d2cd5788076fc0e3465dd2bb18f4230b7bc2
Marco Ilari <marilari88@gmail.com>
2023-07-16 00:34:15 +0200

    Add flote

--- lua/plugins/flote.lua
+++ lua/plugins/flote.lua
@@ -3,7 +3,9 @@ local map = require("keybindings")
 return {
 	"JellyApple102/flote.nvim",
 	config = function()
-		require("flote").setup()
+		require("flote").setup({
+			window_title = false,
+		})
 		map("n", "<leader>l", "<cmd>Flote<cr>")
 	end,
 }
